954966f34a35fca7030e7aaaef055689857c7932
rafai77 <rafac11@hotmail.com>
2019-11-22 05:00:03 +0800

    make it better

--- server.js
+++ server.js
@@ -230,7 +230,7 @@ app.get('/del', (req, res) => {
 app.get('/datos', (req, res) => {
     if(req.session.loggedin)
     {
-    mysqlConnection.query('SELECT * FROM archivos', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM archivos where id=?',idu, (err, rows, fields) => {
         if(!err){
             res.json(rows);
         } else{
